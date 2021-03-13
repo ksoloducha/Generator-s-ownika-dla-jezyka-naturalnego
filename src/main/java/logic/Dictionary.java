8f3e677992eecfacb423be2b634be4964c8db57b
Katarzyna Sołoducha <01142469@pw.edu.pl>
2021-03-13 23:38:18 +0100

    initial commit

---
+++ src/main/java/logic/Dictionary.java
@@ -0,0 +1,73 @@
+package logic;
+
+import java.io.*;
+import java.util.TreeSet;
+
+public class Dictionary implements Serializable {
+
+    private String title;
+    private TreeSet<String> words;
+
+    public Dictionary(String title) {
+        this.title = title;
+        words = new TreeSet<>();
+    }
+
+    public void add(String... words) {
+        for (String word : words) {
+            this.words.add(word);
+        }
+    }
+
+    public void setWords(TreeSet<String> words) {
+        this.words = words;
+    }
+
+    public void saveToFile(String outputFile) throws IOException {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream((new FileOutputStream(outputFile)))) {
+            outputStream.writeObject(this);
+        } catch (FileNotFoundException e1) {
+            throw e1;
+        } catch (IOException e2) {
+            throw e2;
+        }
+    }
+
+    public static Dictionary createFromFile(String inputFile) throws ClassNotFoundException, IOException {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(inputFile))) {
+            Dictionary dictionaryFromFile = (Dictionary) inputStream.readObject();
+            return dictionaryFromFile;
+        } catch (FileNotFoundException | ClassNotFoundException e1) {
+            throw e1;
+        } catch (IOException e2) {
+            throw e2;
+        }
+    }
+
+    public TreeSet<String> getWords(){
+        return words;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    @Override
+    public int hashCode(){
+        int hashCode = title.hashCode() * 17 + words.hashCode() * 23;
+        return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if(other == this){
+            return true;
+        }
+        if(!(other instanceof logic.Dictionary)){
+            return false;
+        }
+        Dictionary otherDictionary = (Dictionary) other;
+
+        return otherDictionary.title.equals(this.title) && otherDictionary.words.equals(this.words);
+    }
+}
---
+++ src/test/java/logic/DictionaryTest.java
@@ -0,0 +1,65 @@
+package logic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DictionaryTest {
+
+    Dictionary dictionary;
+    String path = "src" + File.separator + "test" + File.separator + "java" + File.separator + "logic" + File.separator + "test_files";
+
+    @BeforeEach
+    public void setUp() {
+        dictionary = new Dictionary("Test");
+        dictionary.add("Some", "sample", "words", "to", "test");
+    }
+
+    @Test
+    public void saveToFile_should_saveTitleAndWordsToGivenFile_when_givenCorrectPath() {
+        String outputFileName = path + File.separator + "out.bin";
+        try {
+            dictionary.saveToFile(outputFileName);
+        } catch (FileNotFoundException e1) {
+            System.out.println(e1.getLocalizedMessage());
+        } catch (IOException e2) {
+            System.out.println(e2.getLocalizedMessage());
+        }
+        File testOutput = new File(outputFileName);
+        assertTrue(testOutput.exists());
+    }
+
+    @Test
+    public void saveToFile_should_throwFileNotFountException_when_pathToOutputFileIsIncorrect(){
+        String outputFileName = path + File.separator + "nonexistent_directory" + File.separator + "out.bin";
+        assertThrows(FileNotFoundException.class, () ->{
+            dictionary.saveToFile(outputFileName);
+        });
+    }
+
+    @Test
+    public void createFromFile_should_createDictionaryFromFile_when_correctFileExists(){
+        String outputFileName = path + File.separator + "out.bin";
+        try {
+            dictionary.saveToFile(outputFileName);
+            Dictionary dictionaryFromFile = Dictionary.createFromFile(outputFileName);
+            assertEquals(dictionary, dictionaryFromFile);
+        } catch (FileNotFoundException | ClassNotFoundException e1) {
+            System.out.println(e1.getLocalizedMessage());
+        } catch (IOException e2) {
+            System.out.println(e2.getLocalizedMessage());
+        }
+    }
+
+    @Test
+    public void createFromFile_should_throwFileNotFountException_when_pathToOutputFileIsIncorrect(){
+        String outputFileName = path + File.separator + "nonexistent_directory" + File.separator + "out.bin";
+        assertThrows(FileNotFoundException.class, () ->{
+            dictionary.createFromFile(outputFileName);
+        });
+    }
+}
